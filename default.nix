93277c529ea51cbcb414ccfc3f8f12480c55f98f
nate-wilkins <nate-wilkins@code-null.com>
2026-01-28 11:17:42 -0500

    Release '0.2.0'

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
     export PROGRAM_DIRNAME=${pkgs.coreutils}/bin/dirname
     export PROGRAM_GREP=${pkgs.gnugrep}/bin/grep
     export PROGRAM_HEAD=${pkgs.coreutils}/bin/head
-    export PROGRAM_NMCLI=${pkgs.python313Packages.nmcli}/bin/nmcli
+    export PROGRAM_NMCLI=${pkgs.networkmanager}/bin/nmcli
     export PROGRAM_ROFI=${pkgs.rofi}/bin/rofi
     export PROGRAM_SED=${pkgs.gnused}/bin/sed
     export PROGRAM_UNIQ=${pkgs.coreutils}/bin/uniq
